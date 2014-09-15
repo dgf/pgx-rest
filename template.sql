40ac12a9341967a16116f1281a75291379f64ddb
dgf <deep@dagnu.de>
2014-09-15 02:00:18 +0200

    add route and template HTML

--- template.sql
+++ template.sql
@@ -22,3 +22,35 @@ $$ LANGUAGE plpgsql
    -- Export access for all!
    SECURITY DEFINER;
 
+-- list all templates
+INSERT INTO route (method, path, proc, description) VALUES
+('get', '/templates', 'get_templates', 'list all published templates');
+CREATE FUNCTION get_templates(req request)
+  RETURNS response AS $$ DECLARE templates json;
+  BEGIN
+    WITH template_list AS (
+      SELECT t.*, json_agg(json_build_object(
+        'method', r.method
+      , 'params', r.params
+      , 'path', r.path)
+      ORDER BY r.method, r.path) AS routes
+      FROM template t
+      JOIN route r ON r.proc = t.proc
+      GROUP BY t.id
+    )
+    SELECT json_agg(json_build_object(
+      'path', path
+    , 'proc', proc
+    , 'mime', mime
+    , 'locals', locals
+    , 'routes', routes)
+    ORDER BY t.path, t.mime)
+    FROM template_list t INTO templates;
+    RETURN (200, json_build_object('templates', templates));
+  END;
+$$ LANGUAGE plpgsql;
+
+INSERT INTO template (proc, mime, path, locals) VALUES
+('get_routes'   , 'html', 'routes.html', '{"title":"Public routes API"}'::json),
+('get_templates', 'html', 'templates.html', '{"title":"Published templates"}'::json);
+
