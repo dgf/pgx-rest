fad10e55e0bc62a7062d10485ed1d3ca7b463968
dgf <deep@dagnu.de>
2016-01-25 20:08:29 +0100

    fix public clean

--- clean.sql
+++ clean.sql
@@ -1,5 +1,5 @@
 -- cleanup in reverse order, this also documents the API top down
-DROP FUNCTION IF EXISTS call(m method, c_path text, c_session uuid, body json);
+DROP FUNCTION IF EXISTS call(c_method text, c_path text, c_session uuid, body json);
 DROP FUNCTION IF EXISTS find_template(r_mime text, r_path text, OUT path text, OUT locals json);
 DROP FUNCTION IF EXISTS login(u_login text, u_basic_auth text);
 DROP FUNCTION IF EXISTS post_login(u_login text, u_password text);
